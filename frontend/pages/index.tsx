fea0c2d9654d9ec68df95c9b594de061c17e54ad
preciousnwaoha <nwaohaprecious25@gmail.com>
2024-01-27 16:50:09 +0100

    Setup CreateNewSaleEvent of Frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/layout/header";
+import PaddedContainer from "@/components/layout/padded-container";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,13 +13,22 @@ export default function Home() {
     >
       <Header />
 
-      <div className="border flex flex-col items-center justify-center h-[100vh] ">
-        <h1 className="text-6xl mb-8">Sell Properties</h1>
-        <p className="text-2xl">
-          Sell your properties with the help of our platform
-        </p>
 
-        <button className="btn btn-contained btn-small mt-8 bg-black px-4 py-2 text-white">Get Started</button>
+      <div className="border flex flex-col items-center justify-center h-[100vh] ">
+        <PaddedContainer>
+          <h1 className="text-6xl mb-8">Sell Properties</h1>
+          <p className="text-2xl">
+            Sell your properties with the help of our platform
+          </p>
+
+          
+          <Link href="/new">
+          <button className="btn btn-contained btn-small rounded-md mt-8 bg-black px-4 py-2 text-white">
+            Create
+          </button>
+          </Link>
+        </PaddedContainer>
+        
       </div>
      
     </main>
